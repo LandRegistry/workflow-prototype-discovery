56a54c158e7dee9d148d86542d6bc2d2e6be71dd
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2015-03-23 10:27:23 +0000

    add plans and iterate

--- js/map.js
+++ js/map.js
@@ -9,10 +9,9 @@ var map = new ol.Map({
       collapsible: false
     })
   }),
-  //renderer: exampleNS.getRendererFromQueryString(),
   target: 'map',
   view: new ol.View({
     center: ol.proj.transform([-4.126241, 50.412724], 'EPSG:4326', 'EPSG:3857'),
-    zoom: 18
+    zoom: 19
   })
 });
